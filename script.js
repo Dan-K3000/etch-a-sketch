86048c6a2dd4af94d455022d4235785e451a003c
Dan K <Dan@thefrontdoor.lol>
2022-05-04 21:45:23 -0600

    Add drawing functions
    pixels now change when clicked and change back when clicked a second
    time

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ function createGrid(rows, cols) {
   sketch.style.setProperty('--grid-rows', rows);
   sketch.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement('div');
+    const cell = document.createElement('div');
     sketch.appendChild(cell).classList.add('cell');
   };
 };
 
 createGrid(16, 16);
+
+const pixels = document.querySelectorAll('.cell');
+
+pixels.forEach((pixel) => {
+  pixel.addEventListener('click', (e) => {
+    const pixel = e.target;
+    if(pixel.getAttribute('style') != 'background: black;'){
+      pixel.setAttribute('style', 'background: black;');
+    }
+    else{
+      pixel.removeAttribute('style');
+
+    }
+  });
+});
